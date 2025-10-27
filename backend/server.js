7069b1d932d95199fc3df6fe436baa74a058c9fa
Your Name <devashriya008@gmail.com>
2025-10-27 10:59:10 +0000

    Frontend file connected to backend

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import multer from "multer";
 import { config } from "dotenv";
 import analyzeRoute from "./routes/analyze.js";
 import downloadRoute from "./routes/download.js";
+import path from "path";
 
 config();
 const app = express();
@@ -12,13 +13,19 @@ const upload = multer();
 app.use(cors());
 app.use(express.json());
 
+
+// Serve frontend
+const buildPath = path.join(__dirname, "../frontend/dist");;
+app.use(express.static(buildPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(buildPath, "index.html"));
+});
+  
+// Routes
 app.use("/analyze", analyzeRoute);
 app.use("/download-cover-letter", downloadRoute);
 
-app.get('/', (req, res) => {
-  res.send('Resume Helper API is running.');
-});
-  
 const PORT = process.env.PORT || 3001;
 
 app.listen(process.env.PORT, () => {
